ccf9cfdec769e8070935221a43613b8972b3094b
Nitinsingh99 <8761964643ns@gmail.com>
2025-10-10 20:52:38 +0530

    prog(dsa): solve Q14

--- src/Lec2_BuildUpLogicalThinking/Question14/Q14.java
+++ src/Lec2_BuildUpLogicalThinking/Question14/Q14.java
@@ -1,4 +1,18 @@
 package Lec2_BuildUpLogicalThinking.Question14;
 
 public class Q14 {
+    public static void main(String[] args){
+        printPattern(3);
+    }
+
+    private static void printPattern(int N){
+
+         for(int i=0; i<N; i++){
+             char start = 'A';
+             for(int j=0; j<=i; j++){
+                 System.out.print(start++ + " ");
+             }
+             System.out.println();
+         }
+    }
 }
