a05aca6a22225417b8ef9419b6a5e5f0a398009d
Nitinsingh99 <8761964643ns@gmail.com>
2025-10-10 21:12:35 +0530

    prog(dsa): solve Q16

--- src/Lec2_BuildUpLogicalThinking/Question16/Q16.java
+++ src/Lec2_BuildUpLogicalThinking/Question16/Q16.java
@@ -1,4 +1,18 @@
 package Lec2_BuildUpLogicalThinking.Question16;
 
 public class Q16 {
+    public static void main(String[] args){
+        printPattern(3);
+    }
+
+    private static void printPattern(int N){
+        char start = 'A';
+        for(int i=0; i<N; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(start + " ");
+            }
+            start++;
+            System.out.println();
+        }
+    }
 }
