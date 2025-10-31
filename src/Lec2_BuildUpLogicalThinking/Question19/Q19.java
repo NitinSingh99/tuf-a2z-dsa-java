38d78d360843d44b01d5dda8fa89f473b78319ea
Nitinsingh99 <8761964643ns@gmail.com>
2025-10-31 23:57:20 +0530

    dsa: add q19

--- src/Lec2_BuildUpLogicalThinking/Question19/Q19.java
+++ src/Lec2_BuildUpLogicalThinking/Question19/Q19.java
@@ -1,4 +1,12 @@
 package Lec2_BuildUpLogicalThinking.Question19;
 
 public class Q19 {
+    public static void main(String[] args){
+        int N = 3;
+        printPattern(N);
+    }
+
+    private static void printPattern(int N){
+
+    }
 }
