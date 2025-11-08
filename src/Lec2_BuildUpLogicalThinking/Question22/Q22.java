6fcea9840847ca08661dd8e83a12dfc725894777
Nitinsingh99 <8761964643ns@gmail.com>
2025-11-08 19:47:41 +0530

    patterns: q22

--- src/Lec2_BuildUpLogicalThinking/Question22/Q22.java
+++ src/Lec2_BuildUpLogicalThinking/Question22/Q22.java
@@ -1,4 +1,12 @@
 package Lec2_BuildUpLogicalThinking.Question22;
 
 public class Q22 {
+    public static void main(String[] args){
+        int N = 3;
+        printPattern(N);
+    }
+
+    private static void printPattern(int N){
+
+    }
 }
