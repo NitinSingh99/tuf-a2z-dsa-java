141f53f211711231992abaf74a463b21fa3c7ec9
Nitinsingh99 <8761964643ns@gmail.com>
2025-11-16 23:38:00 +0530

    solve q1 of lec4

--- src/step1/lec4/q1/solution.java
+++ src/step1/lec4/q1/solution.java
@@ -1,4 +1,17 @@
 package step1.lec4.q1;
 
 public class solution {
+    public static void  main(String[] args){
+            int N = 12343223;
+            countDigits(N);
+    }
+
+    private static void countDigits(int N){
+        int digits = 0;
+        while(N>0){
+            N = N/10;
+            digits++;
+        }
+        System.out.println(digits);
+    }
 }
