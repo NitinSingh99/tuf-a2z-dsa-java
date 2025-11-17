9b6d5e10c3f718b4adcacca5c041203be6a6ab56
Nitinsingh99 <8761964643ns@gmail.com>
2025-11-17 23:32:48 +0530

    solve q2

--- src/step1/lec4/q2/solution.java
+++ src/step1/lec4/q2/solution.java
@@ -1,4 +1,19 @@
 package step1.lec4.q2;
 
 public class solution {
+    public static void main(String[] args){
+        int N = 569600;
+        reverse(N);
+    }
+
+    private static void reverse(int N){
+        int answer=0;
+        int ld;
+        while(N>0){
+            ld = N%10;
+            answer = answer*10 + ld;
+            N = N/10;
+        }
+        System.out.println(answer);
+    }
 }
