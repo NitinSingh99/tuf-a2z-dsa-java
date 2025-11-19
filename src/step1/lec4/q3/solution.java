952db6650dee3aca79f31ccd6fd45031f400ee45
Nitinsingh99 <8761964643ns@gmail.com>
2025-11-19 23:14:13 +0530

    solve q3

--- src/step1/lec4/q3/solution.java
+++ src/step1/lec4/q3/solution.java
@@ -1,4 +1,29 @@
 package step1.lec4.q3;
 
 public class solution {
+    public static void main(String[] args){
+        int N = 122322178;
+        checkPalindrome(N);
+    }
+
+    private static void checkPalindrome(int N){
+        int reversed = reverse(N);
+        if(N==reversed){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+
+    private static int reverse(int N){
+        int newNumber = 0;
+        int ld;
+        while(N>0){
+            ld = N%10;
+            newNumber = newNumber*10 + ld;
+            N = N/10;
+        }
+
+        return newNumber;
+    }
 }
