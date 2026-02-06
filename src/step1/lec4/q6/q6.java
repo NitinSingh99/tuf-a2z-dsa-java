abaa13ded37eaf65c21d59516c8dddfb06237f9a
Nitin Singh <8761964643ns@gmail.com>
2026-02-06 21:56:40 +0530

    sol q6

--- src/step1/lec4/q6/q6.java
+++ src/step1/lec4/q6/q6.java
@@ -1,4 +1,26 @@
 package step1.lec4.q6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class q6 {
+    public static void main(String[] args){
+        int N = 12;
+        List<Integer> result = getDivisors(N);
+        System.out.print(result);
+    }
+
+    private static List<Integer> getDivisors(int N){
+        List<Integer> resultArray = new ArrayList<>();
+
+        for(int i=1; i*i<=N; i++){
+            if(N%i==0){
+                resultArray.add(i);
+                if(i != N/i){
+                    resultArray.add(N/i);
+                }
+            }
+        }
+        return resultArray;
+    }
 }
