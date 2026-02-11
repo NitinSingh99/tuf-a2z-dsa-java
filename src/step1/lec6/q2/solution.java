f986e1df2307a9bf96d94acb760ea8602c7bdd47
Nitin Singh <8761964643ns@gmail.com>
2026-02-12 00:12:54 +0530

    solve q2

---
+++ src/step1/lec6/q2/solution.java
@@ -0,0 +1,24 @@
+package step1.lec6.q2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class solution {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 10, 15, 10, 5};
+        int n = arr.length;
+        freq(arr, n);
+    }
+
+    private static void freq(int[] arr, int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue() );
+        }
+    }
+}
