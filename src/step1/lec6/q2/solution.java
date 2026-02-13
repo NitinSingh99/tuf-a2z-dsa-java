3acbc8e524fae1f054ac3f654c5670d09149edea
Nitin Singh <8761964643ns@gmail.com>
2026-02-13 22:25:04 +0530

    practive hasing

--- src/step1/lec6/q2/solution.java
+++ src/step1/lec6/q2/solution.java
@@ -21,4 +21,16 @@ public class solution {
             System.out.println(entry.getKey() + " " + entry.getValue() );
         }
     }
+
+    //Practice 01
+    private static void freqq(int[] arr, int n){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
 }
