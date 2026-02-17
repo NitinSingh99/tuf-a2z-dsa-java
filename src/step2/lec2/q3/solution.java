51b2828b3ef297ced8c6a31a1c87bdf6ba60292e
Nitin Singh <8761964643ns@gmail.com>
2026-02-18 00:06:19 +0530

    solve q3

--- src/step2/lec2/q3/solution.java
+++ src/step2/lec2/q3/solution.java
@@ -1,4 +1,25 @@
 package step2.lec2.q3;
 
+import java.util.Arrays;
+
 public class solution {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        insertionSort(arr, 1);
+        System.out.print(Arrays.toString(arr));
+    }
+
+    private static void insertionSort(int[] arr, int ind) {
+        if (ind == arr.length) return;
+        int cur = arr[ind];
+        int i = ind - 1;
+
+        while (i >= 0 && arr[i] > cur) {
+            arr[i + 1] = arr[i];
+            i--;
+        }
+
+        arr[i+1] = cur;
+        insertionSort(arr, ind + 1);
+    }
 }
