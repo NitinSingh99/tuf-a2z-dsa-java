7634de5df97731b96a111acf79185c0675833c56
Nitin Singh <8761964643ns@gmail.com>
2026-02-21 21:36:37 +0530

    revisit

--- src/step1/lec2/Question14/Q14.java
+++ src/step1/lec2/Question14/Q14.java
@@ -1,18 +1,30 @@
 package step1.lec2.Question14;
 
 public class Q14 {
-    public static void main(String[] args){
-        printPattern(3);
+    public static void main(String[] args) {
+        printPattern_rev(3);
     }
 
-    private static void printPattern(int N){
+    private static void printPattern(int N) {
 
-         for(int i=0; i<N; i++){
-             char start = 'A';
-             for(int j=0; j<=i; j++){
-                 System.out.print(start++ + " ");
-             }
-             System.out.println();
-         }
+        for (int i = 0; i < N; i++) {
+            char start = 'A';
+            for (int j = 0; j <= i; j++) {
+                System.out.print(start++ + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    //Revision 1
+    private static void printPattern_rev(int N) {
+
+        for (int i = 1; i <= N; i++) {
+            char init = 'A';
+            for (int j = 1; j <= i; j++) {
+                System.out.print(init++);
+            }
+            System.out.println();
+        }
     }
 }
