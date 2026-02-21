bab1c1b1b124062b9c99d551207726a89912338f
Nitin Singh <8761964643ns@gmail.com>
2026-02-21 22:28:54 +0530

    revisit

--- src/step1/lec2/Question7/Q7.java
+++ src/step1/lec2/Question7/Q7.java
@@ -2,7 +2,7 @@ package step1.lec2.Question7;
 
 public class Q7 {
     public static void main(String[] args){
-        printPattern(7);
+        printPattern_rev(7);
     }
 
     private static void printPattern(int n){
@@ -12,6 +12,21 @@ public class Q7 {
             }
 
             for(int x=1; x<=2*i-1; x++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    //Revision - X
+    private static void printPattern_rev(int N){
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<= N-i; j++){
+                System.out.print(" ");
+            }
+
+            for(int k = 1; k<=2*i-1; k++){
                 System.out.print("*");
             }
 
