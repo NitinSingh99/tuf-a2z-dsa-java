916403f8e3fd9c22a648f15dab6a510fcad1c5d4
Nitin Singh <8761964643ns@gmail.com>
2026-02-22 12:55:46 +0530

    revisit

--- src/step1/lec4/q1/optimal_solution.java
+++ src/step1/lec4/q1/optimal_solution.java
@@ -3,11 +3,17 @@ package step1.lec4.q1;
 public class optimal_solution {
     public static void main(String[] args){
         int N = 232323;
-        countDigits(N);
+        countDigits_rev(N);
     }
 
     private static void countDigits(int N){
         int cnt = (int) (Math.log10(N) + 1);
         System.out.println(cnt);
+    }
+
+    //Revisit 1 -- O
+    private static void countDigits_rev(int N){
+        int cnt = (int) (Math.log10(N) + 1);
+        System.out.print(cnt);
     }
 }
