47937546fb528eb16c326165efc5a45ff86f2618
Nitin Singh <8761964643ns@gmail.com>
2026-02-28 12:33:42 +0530

    revisit

--- src/step1/lec6/q3/solution.java
+++ src/step1/lec6/q3/solution.java
@@ -1,5 +1,6 @@
 package step1.lec6.q3;
 
+import java.net.Inet4Address;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,7 +8,7 @@ public class solution {
     public static void main(String[] args) {
         int[] arr = {10, 5, 10, 15, 10, 5};
         int n = arr.length;
-        freq(arr, n);
+        freq_rev(arr, n);
     }
 
     private static void freq(int[] arr, int n) {
@@ -32,5 +33,33 @@ public class solution {
         }
 
         System.out.print("Lowest: " + lInd + ", Highest: " + hInd);
+    }
+
+    //Revisit 1 -- O
+    private static void freq_rev(int[] arr, int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        int max = Integer.MIN_VALUE;
+        int maxEl = 0;
+        int minEl = 0;
+        int min = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                maxEl = entry.getKey();
+            }
+
+            if(entry.getValue() < min){
+                min = entry.getValue();
+                minEl = entry.getKey();
+            }
+
+        }
+
+        System.out.println("Max: " + maxEl);
+        System.out.println("Min: " + minEl);
     }
 }
