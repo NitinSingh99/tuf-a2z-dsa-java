48966f0954b52ce7761a8ec05a3a0ea8aca0d5a4
Nitin Singh <8761964643ns@gmail.com>
2026-02-28 12:23:31 +0530

    revisit

--- src/step1/lec6/q2/solution.java
+++ src/step1/lec6/q2/solution.java
@@ -8,7 +8,7 @@ public class solution {
     public static void main(String[] args) {
         int[] arr = {10, 5, 10, 15, 10, 5};
         int n = arr.length;
-        freq(arr, n);
+        freq_rev(arr, n);
     }
 
     private static void freq(int[] arr, int n) {
@@ -26,6 +26,18 @@ public class solution {
     private static void freqq(int[] arr, int n){
         Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<n; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    //Revisit 2 -- O
+    private static void freq_rev(int[] arr, int n){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<n; i++){
             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
 
