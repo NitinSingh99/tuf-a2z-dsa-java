e0c2db33a7534133910091c2c303a4fc362f4849
Nitin Singh <8761964643ns@gmail.com>
2026-02-28 20:35:25 +0530

    revisit

--- src/b_sorting/a_selectionSort/solution.java
+++ src/b_sorting/a_selectionSort/solution.java
@@ -5,9 +5,14 @@ import java.util.Arrays;
 public class solution {
     public static void main(String[] args) {
         int[] arr = {13, 46, 24, 52, 20, 9};
-        selectionSort2(arr);
+        selectionSort3(arr);
     }
 
+    /*
+    Attempt: 1
+    Status: SOLVED
+    Confidence: 7/10
+    */
     private static void selectionSort(int[] arr) {
         int counter = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -24,7 +29,11 @@ public class solution {
         System.out.print(Arrays.toString(arr));
     }
 
-    //Practice
+    /*
+    Attempt: 2
+    Status: SOLVED
+    Confidence: 7/10
+    */
     private static void selectionSort2(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n; i++) {
@@ -38,6 +47,28 @@ public class solution {
             }
             arr[i] = min;
         }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /*
+    Attempt: 3
+    Status: SOLVED
+    Confidence: 8/10
+    Date: 28-02-2026
+    */
+    private static void selectionSort3(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
         System.out.println(Arrays.toString(arr));
     }
 }
