7bbc7ef8abe32ba24f8d2f3278ba4b6e848c332b
Nitin Singh <8761964643ns@gmail.com>
2026-03-01 22:06:24 +0530

    revisit

--- src/a_basics/c_recursion/e_factorial/solution.java
+++ src/a_basics/c_recursion/e_factorial/solution.java
@@ -1,14 +1,24 @@
 package a_basics.c_recursion.e_factorial;
 
 public class solution {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int N = 3;
-        int result = factorial(N);
+        int result = factorial2(N);
         System.out.print(result);
     }
 
-    private static int factorial(int N){
-        if(N==0) return 1;
+    private static int factorial(int N) {
+        if (N == 0) return 1;
         return N * factorial(--N);
+    }
+
+    /*
+    Attempt: 2
+    Status: SOLVED
+    Confidence: 9/10
+    */
+    private static int factorial2(int N) {
+        if (N <= 1) return 1;
+        return N * factorial2(N-1);
     }
 }
