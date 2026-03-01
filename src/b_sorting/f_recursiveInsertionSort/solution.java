655aba24f93b53ba51ed65138e7d2c5b82b4f64b
Nitin Singh <8761964643ns@gmail.com>
2026-03-01 11:27:53 +0530

    revisit

--- src/b_sorting/f_recursiveInsertionSort/solution.java
+++ src/b_sorting/f_recursiveInsertionSort/solution.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class solution {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
-        insertionSort(arr, 1);
+        insertionSort2(arr, 1);
         System.out.print(Arrays.toString(arr));
     }
 
@@ -19,7 +19,27 @@ public class solution {
             i--;
         }
 
-        arr[i+1] = cur;
+        arr[i + 1] = cur;
         insertionSort(arr, ind + 1);
+    }
+
+    /*
+    Attempt: 2
+    Status: SOLVED
+    Confidence: 6/10
+    */
+    private static void insertionSort2(int[] arr, int ind) {
+        if (ind == arr.length) return;
+        int curr = arr[ind];
+        int i = ind - 1;
+
+        while (i >= 0 && arr[i] > curr) {
+            arr[i + 1] = arr[i];
+            i--;
+        }
+
+        arr[i + 1] = curr;
+
+        insertionSort2(arr, ind + 1);
     }
 }
