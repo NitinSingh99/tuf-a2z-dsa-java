7821bf84bda87a340d348b68d6cd6d2ad7403ab2
Nitin Singh <8761964643ns@gmail.com>
2026-03-01 13:04:43 +0530

    solve quick sort

--- src/b_sorting/g_quickSort/solution.java
+++ src/b_sorting/g_quickSort/solution.java
@@ -1,4 +1,46 @@
 package b_sorting.g_quickSort;
 
+import java.util.Arrays;
+
 public class solution {
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 7, 9, 3};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low >= high) return;
+        int partitionInd = partition(arr, low, high);
+        quickSort(arr, low, partitionInd - 1);
+        quickSort(arr, partitionInd + 1, high);
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (arr[i] <= pivot && i <= high - 1) {
+                i++;
+            }
+
+            while (arr[j] > pivot && j >= low + 1) {
+                j--;
+            }
+
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+
+        return j;
+    }
 }
