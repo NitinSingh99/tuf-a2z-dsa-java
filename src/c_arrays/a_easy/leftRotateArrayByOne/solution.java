a90ec4c72c33eb3c38dc8f33382f0c653f681330
Nitin Singh <8761964643ns@gmail.com>
2026-03-02 21:34:14 +0530

    sovle rotateArrayByOne

---
+++ src/c_arrays/a_easy/leftRotateArrayByOne/solution.java
@@ -0,0 +1,24 @@
+package c_arrays.a_easy.leftRotateArrayByOne;
+
+import java.util.Arrays;
+
+public class solution {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        rotate(arr, 1);
+    }
+
+    /*
+    Attempt: 1
+    Status: SOLVED
+    Confidence: 8/10
+    */
+    private static void rotate(int[] arr, int n) {
+        int temp = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i - 1] = arr[i];
+        }
+        arr[arr.length - 1] = temp;
+        System.out.println(Arrays.toString(arr));
+    }
+}
