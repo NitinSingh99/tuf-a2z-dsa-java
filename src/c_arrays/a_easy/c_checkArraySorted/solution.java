bfd592b48ae7de4c65f0d2f8ca3ed04142a41b50
Nitin Singh <8761964643ns@gmail.com>
2026-03-07 20:32:12 +0530

    revisit

--- src/c_arrays/a_easy/c_checkArraySorted/solution.java
+++ src/c_arrays/a_easy/c_checkArraySorted/solution.java
@@ -26,7 +26,7 @@ public class solution {
     Attempt: 2
     Status: SOLVED
     Confidence: 9/10
-    Date: 01-03-26
+    Date: 07-03-26
     */
     private static boolean check2(int[] arr){
         for(int i = 1; i<arr.length; i++){
--- src/c_arrays/a_easy/d_removeDuplicatesFromArray/solution.java
+++ src/c_arrays/a_easy/d_removeDuplicatesFromArray/solution.java
@@ -2,11 +2,13 @@ package c_arrays.a_easy.d_removeDuplicatesFromArray;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class solution {
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 2, 2, 3, 3};
-        int ind = removeDup(arr);
+        int ind = removeDup2(arr);
         for(int i = 0; i<=ind; i++){
             System.out.print(arr[i] + " ");
         }
@@ -21,10 +23,28 @@ public class solution {
     private static int removeDup(int[] arr) {
         if (arr.length == 0) return 0;
 
-        ArrayList<Integer> temp = new ArrayList<>();
         int j = 0;
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+
+        return j;
+    }
+
+    /*
+    Attempt: 2
+    Status: --
+    Confidence: 6/10
+    Date: 01-03-26
+    */
+    private static int removeDup2(int[] arr){
+        if(arr.length == 0) return 0;
+        int j = 0;
+        for(int i = 1; i<arr.length; i++){
+            if(arr[i] != arr[j]){
                 j++;
                 arr[j] = arr[i];
             }
