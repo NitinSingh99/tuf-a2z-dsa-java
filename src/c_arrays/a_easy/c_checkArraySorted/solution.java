d3545525d15469fd37633e48301f5f3698f8eeb4
Nitin Singh <8761964643ns@gmail.com>
2026-03-07 20:09:59 +0530

    revisit

--- src/c_arrays/a_easy/c_checkArraySorted/solution.java
+++ src/c_arrays/a_easy/c_checkArraySorted/solution.java
@@ -2,8 +2,8 @@ package c_arrays.a_easy.c_checkArraySorted;
 
 public class solution {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 6, 5};
-        boolean res = check(arr);
+        int[] arr = {1, 2, 3, 4, 5};
+        boolean res = check2(arr);
         System.out.println(res);
     }
 
@@ -16,6 +16,21 @@ public class solution {
     private static boolean check(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*
+    Attempt: 2
+    Status: SOLVED
+    Confidence: 9/10
+    Date: 01-03-26
+    */
+    private static boolean check2(int[] arr){
+        for(int i = 1; i<arr.length; i++){
+            if(arr[i]<arr[i-1]){
                 return false;
             }
         }
