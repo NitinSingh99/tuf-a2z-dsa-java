1366e97c014016412ac4faf5e49f9d51bd86af51
Nitin Singh <8761964643ns@gmail.com>
2026-03-08 21:42:30 +0530

    revisit

--- src/c_arrays/a_easy/j_maxConsecutiveOnes/solution.java
+++ src/c_arrays/a_easy/j_maxConsecutiveOnes/solution.java
@@ -3,7 +3,7 @@ package c_arrays.a_easy.j_maxConsecutiveOnes;
 public class solution {
     public static void main(String[] args) {
         int[] arr = {1, 1, 0, 1, 1, 1};
-        max(arr);
+        max_rev(arr);
     }
 
     /*
@@ -22,5 +22,25 @@ public class solution {
             count = Math.max(max, count);
         }
         System.out.println(count);
+    }
+
+    /*
+    Attempt: 2
+    Status: SOLVED
+    Confidence: 8/10
+    */
+    private static void max_rev(int[] arr) {
+        int cnt = 0;
+        int max = 0;
+        for (int j : arr) {
+            if (j == 1) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+            max = Math.max(max, cnt);
+        }
+
+        System.out.println(max);
     }
 }
