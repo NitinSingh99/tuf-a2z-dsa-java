d32a392f58320802733b77eb5ba8930c5e314b6d
Nitin Singh <8761964643ns@gmail.com>
2026-04-11 16:37:03 +0530

    revisit

--- src/c_arrays/b_medium/d_maxSubArrSum/solution.java
+++ src/c_arrays/b_medium/d_maxSubArrSum/solution.java
@@ -3,7 +3,7 @@ package c_arrays.b_medium.d_maxSubArrSum;
 public class solution {
     public static void main(String[] args) {
         int[] arr = {-2, -3, -7, -2, -10, -4};
-        maxSum(arr);
+        maxSum_rev(arr);
     }
 
     private static void maxSum(int[] arr) {
@@ -12,6 +12,19 @@ public class solution {
             sum += num;
             max = Math.max(max, sum);
             if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        System.out.println(max);
+    }
+
+    private static void maxSum_rev(int[] arr){
+        int sum = 0, max = Integer.MIN_VALUE;
+        for(int num : arr){
+            sum += num;
+            max = Math.max(max, sum);
+            if(sum < 0){
                 sum = 0;
             }
         }
