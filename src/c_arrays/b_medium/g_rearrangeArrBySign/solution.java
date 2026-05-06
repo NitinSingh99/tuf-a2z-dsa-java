13020940cdd4c2be94ca8b53687e46865b55d8fc
Nitin Singh <8761964643ns@gmail.com>
2026-05-06 23:27:27 +0530

    revisit

--- src/c_arrays/b_medium/g_rearrangeArrBySign/solution.java
+++ src/c_arrays/b_medium/g_rearrangeArrBySign/solution.java
@@ -1,12 +1,40 @@
 package c_arrays.b_medium.g_rearrangeArrBySign;
 
+import java.util.Arrays;
+
 public class solution {
-    public static void main(String[] args){
-        int[] arr = {1,2,-4,-5};
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, -1, -2, -3};
         arrange(arr);
     }
 
-    private static void arrange(int[] arr){
-
+    //my solution works but is fragile fails for edge cases like when arr = {1, 2, 3, -1, -2, -3}
+    private static void arrange(int[] arr) {
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[j+1] > 0 && arr[j] > 0){
+                if(arr[i] < 0){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[i];
+                    arr[i] = temp;
+                    j = i;
+                }
+            }else if(arr[j+1] < 0 && arr[j] < 0){
+                if(arr[i] > 0){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[i];
+                    arr[i] = temp;
+                    j = i;
+                }
+            }else{
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
+
+//    private static void rearrange(int[] arr){
+//        int n = arr.length;
+//
+//    }
 }
