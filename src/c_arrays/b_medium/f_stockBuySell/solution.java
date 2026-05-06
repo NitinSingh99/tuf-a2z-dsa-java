1098849623bf6480f6c94df486d498ad56322546
Nitin Singh <8761964643ns@gmail.com>
2026-05-06 22:15:13 +0530

    revisit

--- src/c_arrays/b_medium/f_stockBuySell/solution.java
+++ src/c_arrays/b_medium/f_stockBuySell/solution.java
@@ -3,19 +3,33 @@ package c_arrays.b_medium.f_stockBuySell;
 public class solution {
     public static void main(String[] args) {
         int[] arr = {7, 1, 5, 3, 6, 4};
-        buySell(arr);
+        buySell_rev(arr);
     }
 
     private static void buySell(int[] arr) {
         int min = Integer.MAX_VALUE;
         int profit = 0;
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] < min){
+            if (arr[i] < min) {
                 min = arr[i];
-            }else{
+            } else {
                 profit = Math.max(profit, arr[i] - min);
             }
         }
         System.out.println(profit);
+    }
+
+    private static void buySell_rev(int[] arr) {
+        int buy = arr[0];
+        int max = 0;
+        for (int i = 1; i < arr.length; i++) {
+
+            if(arr[i] < buy){
+                buy = arr[i];
+            }else{
+                max = Math.max(max, arr[i] - buy);
+            }
+        }
+        System.out.println(max);
     }
 }
---
+++ src/c_arrays/b_medium/g_rearrangeArrBySign/solution.java
@@ -0,0 +1,12 @@
+package c_arrays.b_medium.g_rearrangeArrBySign;
+
+public class solution {
+    public static void main(String[] args){
+        int[] arr = {1,2,-4,-5};
+        arrange(arr);
+    }
+
+    private static void arrange(int[] arr){
+
+    }
+}
