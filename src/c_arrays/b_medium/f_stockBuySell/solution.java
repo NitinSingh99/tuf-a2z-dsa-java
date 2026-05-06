caea1de3e250d94d6854290867d6cadc7a4b029d
Nitin Singh <8761964643ns@gmail.com>
2026-05-06 22:19:50 +0530

    revisit

--- src/c_arrays/b_medium/f_stockBuySell/solution.java
+++ src/c_arrays/b_medium/f_stockBuySell/solution.java
@@ -3,7 +3,7 @@ package c_arrays.b_medium.f_stockBuySell;
 public class solution {
     public static void main(String[] args) {
         int[] arr = {7, 1, 5, 3, 6, 4};
-        buySell_rev(arr);
+        buySell_rev2(arr);
     }
 
     private static void buySell(int[] arr) {
@@ -24,12 +24,26 @@ public class solution {
         int max = 0;
         for (int i = 1; i < arr.length; i++) {
 
-            if(arr[i] < buy){
+            if (arr[i] < buy) {
                 buy = arr[i];
-            }else{
+            } else {
                 max = Math.max(max, arr[i] - buy);
             }
         }
         System.out.println(max);
+    }
+
+    private static void buySell_rev2(int[] arr) {
+        int buy = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < buy){
+                buy = arr[i];
+            }else{
+                profit = Math.max(profit, arr[i] - buy);
+            }
+        }
+
+        System.out.println(profit);
     }
 }
